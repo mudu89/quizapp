ab13bc7ce250cba3ddbb983a94674f511c218fb5
mudu89 <40826484+mudu89@users.noreply.github.com>
2025-04-25 17:13:23 +0530

    Update config.ts

--- my-react-app/src/components/common/config.ts
+++ my-react-app/src/components/common/config.ts
@@ -1,5 +1,12 @@
+console.log("Fetching API URL from environment variables")
+console.log(process.env)
+
 const APP_ENV = process.env.REACT_APP_APP_ENV || 'development'; // Default to 'development'
 
-export const API_URL = APP_ENV === 'production' ? process.env.API_URL : 'http://127.0.0.1:8000';
+console.log('APP_ENV:', APP_ENV);
+console.log('API_URL:', process.env.REACT_APP_API_URL);
+console.log('WS_URL:', process.env.REACT_APP_WS_URL);
+
+export const API_URL = APP_ENV === 'production' ? process.env.REACT_APP_API_URL : 'http://127.0.0.1:8000';
   
-export const WS_URL = APP_ENV === 'production' ? process.env.WS_URL : 'ws://127.0.0.1:8000/ws';
+export const WS_URL = APP_ENV === 'production' ? process.env.REACT_APP_WS_URL : 'ws://127.0.0.1:8000/ws';
